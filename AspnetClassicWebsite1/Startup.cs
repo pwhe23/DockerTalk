1bc754d699cc6b488e379a35e0137dadace8c7d7
Paul Wheeler <paul@paulwheeler.com>
2017-01-04 14:53:19 -0500

    add /AspnetClassicWebsite1/

---
+++ AspnetClassicWebsite1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspnetClassicWebsite1.Startup))]
+namespace AspnetClassicWebsite1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
